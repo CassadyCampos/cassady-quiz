d05ca2ca088ead8f264a0946c9e47645cef37a16
Cassady <cassady801@gmail.com>
2019-08-11 15:24:15 -0600

    Possible Answer selection implemented

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -1,114 +1,123 @@
-import * as React from 'react';
-import { Component } from 'react';
-import Question from './Question';
-import axios from 'axios';
+import * as React from "react";
+import { Component } from "react";
+import Question from "./Question";
+import axios from "axios";
+import "../css/Game.css";
 
-
-interface QuestionState  {
-    question: [{
-        category: string,
-        type: string,
-        difficulty: string,
-        question: string,
-        correct_answer: string,
-        incorrect_answers: string[];
-    }],
-    isLoaded: boolean,
-    possibleAnswers: string[],
-    selectedAnswer: string
+interface QuestionState {
+  question: [
+    {
+      category: string;
+      type: string;
+      difficulty: string;
+      question: string;
+      correct_answer: string;
+      incorrect_answers: string[];
+    }
+  ];
+  isLoaded: boolean;
+  possibleAnswers: string[];
+  selectedAnswer: string;
 }
 
 class Game extends Component<{}, QuestionState> {
-    constructor(props: any) {
-        super(props);
+  constructor(props: any) {
+    super(props);
 
-        this.state = {
-            question: [{category: '',
-                type: '',
-                difficulty: '',
-                question: '',
-                correct_answer: '',
-                incorrect_answers: []
-            }],
-            isLoaded: false,
-            possibleAnswers: [],
-            selectedAnswer: ''
+    this.state = {
+      question: [
+        {
+          category: "",
+          type: "",
+          difficulty: "",
+          question: "",
+          correct_answer: "",
+          incorrect_answers: []
         }
+      ],
+      isLoaded: false,
+      possibleAnswers: [],
+      selectedAnswer: ""
+    };
+  }
+
+  componentDidMount() {
+    this.populateQuestions();
+  }
+  populateQuestions() {
+    axios.get(`https://opentdb.com/api.php?amount=1`).then(res => {
+      let question = res.data.results;
+      this.setState({
+        question: question
+      });
+      this.setState({});
+      this.populatePossibleAnswers();
+      this.shufflePossibleAnswers();
+    });
+  }
+
+  populatePossibleAnswers() {
+    this.setState({
+      possibleAnswers: [this.state.question[0].correct_answer]
+    });
+    this.state.question[0].incorrect_answers.forEach(answer => {
+      this.setState({
+        possibleAnswers: [...this.state.possibleAnswers, answer]
+      });
+    });
+  }
+
+  renderQuestion() {
+    return (
+      <div>
+        <h1>Here is your Question. . .</h1>
+        <div>
+          <div>{this.state.question[0].question}</div>
+        </div>
+      </div>
+    );
+  }
+
+  shufflePossibleAnswers() {
+    let temp = this.state.possibleAnswers.slice();
+
+    for (let i = temp.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [temp[i], temp[j]] = [temp[j], temp[i]];
     }
+    this.setState({ possibleAnswers: temp });
+  }
 
-    componentDidMount() {
-        this.populateQuestions();
-    }
-    populateQuestions() {
-        axios.get(`https://opentdb.com/api.php?amount=1`)
-        .then(res => {
-            let question = res.data.results;
-            console.log(question);
-            this.setState({
-                question: question
-            })
-            this.setState({
-            })
-            this.populatePossibleAnswers();
-        this.shufflePossibleAnswers();
+  selectAnswer(selectedAnswer: string) {
+    this.setState({ selectedAnswer: selectedAnswer });
+  }
 
-        })
-    }
-
-    populatePossibleAnswers() {
-        this.setState({
-            possibleAnswers: [this.state.question[0].correct_answer]
-        })
-        this.state.question[0].incorrect_answers.forEach(answer => {
-            this.setState({
-                possibleAnswers: [...this.state.possibleAnswers, answer]
-            })
-        })
-    }
-
-    renderQuestion() {
-        return (
-            <div>
-                <h1>Here is your Question. . .</h1>
-                <div>
-                    <div>{this.state.question[0].question}</div>
-                </div>
-
-            </div>
-        )
-    }
-
-    shufflePossibleAnswers() {
-        let temp = this.state.possibleAnswers.slice();
-
-        for (let i = temp.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [temp[i], temp[j]] = [temp[j], temp[i]];
-        }
-        this.setState({possibleAnswers: temp})
-    }
+  renderPossibleAnswers() {
 
 
-    renderPossibleAnswers() {
-        console.log(this.state.question[0].correct_answer);
-        return(
-            <div>
-                {this.state.possibleAnswers.map(possibleAnswer => 
-                    <div>{possibleAnswer}</div>)}
-            </div>
-        )
-    }
+    return (
+      <div>
+        {this.state.possibleAnswers.map(possibleAnswer => (
+          <div
+            className={this.state.selectedAnswer === possibleAnswer ? 'selected-answer': 'possible-answer'}
+            onClick={() => this.selectAnswer(possibleAnswer)}
+            key={this.state.possibleAnswers.indexOf(possibleAnswer)}>
+            {possibleAnswer}
+          </div>
+        ))}
+      </div>
+    );
+  }
 
-    render() {
-        return(
 
-            <div>
-                {this.renderQuestion()}
-                {this.renderPossibleAnswers()}
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div>
+        {this.renderQuestion()}
+        {this.renderPossibleAnswers()}
+      </div>
+    );
+  }
 }
 
 export default Game;
